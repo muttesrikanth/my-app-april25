f87b622017382b498e47e1cb0749c5d08f1dc63c
muttesrikanth <srikanth12cn27@gmail.com>
2025-05-21 15:08:11 +0530

    text-area task implimented

--- src/app/text-area/text-area.component.ts
+++ src/app/text-area/text-area.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-text-area',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./text-area.component.css']
 })
 export class TextAreaComponent {
+@Input() max:number=0;
+term:string=''
 
 }
